7ad96a4afa1ef103df314e236d7cbc2cc5810b3e
Jan-Markus Langer <janmarkuslanger10121994@gmail.com>
2023-12-30 13:15:45 +0000

    Fix packages

--- styled.d.ts
+++ styled.d.ts
@@ -2,5 +2,9 @@ import 'styled-components';
 import { StyledBreakpointsTheme } from 'styled-breakpoints';
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends StyledBreakpointsTheme {}
+    export interface DefaultTheme extends StyledBreakpointsTheme {
+        colors: any;
+        fonts: any;
+        maxWidth: any;
+    }
 }
